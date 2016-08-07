7d9d17151efb909e83ba0a9e7370b399e9f90404
xgqfrms <xgqfrms@outlook.com>
2016-08-07 11:27:47 +0800

    UTF-8 for Python
    #!/usr/bin/python
    #  -*- coding: utf-8 -*-
    # from django.shortcuts import render
    from django.http import HttpResponse
    # Create your views here.
    def index(request):
    return HttpResponse('<h1>Hello, this is the first page of my Django(迪亚戈)
    Web App!</h1>')
    #   return HttpResponse('<h1>Hello, this is the first page of my
    Django(迪亚戈) Web App!</h1>')

--- DjangoApp/main_app/urls.py
+++ DjangoApp/main_app/urls.py
@@ -1,5 +1,6 @@
 import django.conf.urls
 from . import views
+# . wildcard character ?
 
 urlpatterns = [
     # localhost/index
--- DjangoApp/main_app/views.py
+++ DjangoApp/main_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+# from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello, this is the first page of my Django Web App!</h1>')
+    return HttpResponse('<h1>Hello, this is the first page of my Django(迪亚戈) Web App!</h1>')
+#   return HttpResponse('<h1>Hello, this is the first page of my Django(迪亚戈) Web App!</h1>')
