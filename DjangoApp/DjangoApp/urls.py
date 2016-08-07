c6582afda689d9c61355ef20bacf9413c5f808f2
xgqfrms <xgqfrms@outlook.com>
2016-08-07 10:23:49 +0800

    ok

--- DjangoApp/DjangoApp/urls.py
+++ DjangoApp/DjangoApp/urls.py
@@ -18,4 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # localhost/index
+    # url(r'^index/', views.index),
+    url(r'^',
+        include('main_app.urls')),
 ]
---
+++ DjangoApp/main_app/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'xray'
+import django.conf.urls
+from . import views
+
+urlpatterns = [
+    # localhost/index
+    django.conf.urls.url(r'^$', views.index),
+]
