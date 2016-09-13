533811b1a905d927d79a9feede9ea8867b68dd04
xgqfrms <xgqfrms@outlook.com>
2016-09-13 20:24:07 +0800

    PEP 8 格式缩进的问题
    https://www.python.org/dev/peps/pep-0008/
    PEP 8 格式缩进的问题

--- spider/wiki_baike/html_outputer.py
+++ spider/wiki_baike/html_outputer.py
@@ -4,10 +4,27 @@ __author__ = 'xray'
 
 class HtmlOutputer(object):
     def __init__(self):
-        self
+        self.datas = []
 
-    def collect_data(self):
-        pass
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
 
     def output_html(self):
-        pass
+        file_out = open('output.html', 'w')
+        file_out.write('<html>')
+        file_out.write('<body>')
+        file_out.write('<table>')
+        for data in self.datas:
+            file_out.write('<tr>')
+            # assic
+            file_out.write('<td>%</td>' % data['url'].encode('utf-8'))
+            file_out.write('<td>%</td>' % data['title'].encode('utf-8'))
+            file_out.write('<td>%</td>' % data['summary'].encode('utf-8'))
+            file_out.write('</tr>')
+        file_out.write('</table>')
+        file_out.write('</body>')
+        file_out.write('</html>')
+
+        file_out.close()
