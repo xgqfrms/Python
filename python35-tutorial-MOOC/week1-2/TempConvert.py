44a8b1820483a7956eb73352f1e019cca4df9885
xgqfrms <xgqfrms@outlook.com>
2016-09-19 16:15:04 +0800

    ok

---
+++ python35-tutorial-MOOC/week1-2/TempConvert.py
@@ -0,0 +1,27 @@
+__author__ = 'xray'
+
+
+# TempConvert.py
+val = input("请输入带温度表示符号的温度值(例如: 32C): ")
+if val[-1] in ['C', 'c']:
+    f = 1.8 * float(val[0:-1]) + 32
+    print("转换后的温度为: %.2fF" % f)
+elif val[-1] in ['F', 'f']:
+    c = (float(val[0:-1]) - 32) / 1.8
+    print("转换后的温度为: %.2fC" % c)
+else:
+    print("输入有误")
+
+('\n'
+ '   # elif !== else if\n'
+ '   #_*_ coding:cp936_*_\n'
+ '## coding: utf8\n'
+ '## coding: gbk\n'
+)
+
+'''
+   # elif !== else if
+   #_*_ coding:cp936_*_
+## coding: utf8
+## coding: gbk
+'''
