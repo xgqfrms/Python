1defa324d89610d9383be94ea179ff5e5ada413c
xgqfrms <xgqfrms@outlook.com>
2016-09-25 17:52:31 +0800

    ok

---
+++ python35-tutorial-MOOC/Python_data/week 02/print doc.py
@@ -0,0 +1,23 @@
+# coding: utf8
+
+__author__ = 'xray'
+
+import math
+print(pow.__doc__)
+print(math.__spec__)
+
+print(help(math.pow))
+print(help(math))
+
+'''
+查看帮助文档方式：
+help
+__doc__
+__spac__
+'''
+
+
+
+
+
+
