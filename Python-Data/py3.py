d50f9bc3ba122e64f6212328993493187dd11a34
xgqfrms <xgqfrms@outlook.com>
2020-08-04 01:43:20 +0800

    python3 vscode snippets

---
+++ Python-Data/py3.py
@@ -0,0 +1,21 @@
+# coding: utf8
+
+__author__ = 'xgqfrms'
+
+"""
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-01-0
+ *
+ * @description
+ * @augments
+ * @example
+ * @link
+ *
+ */
+
+"""
