4d63eb501f17aab14ec6d4c7b3548053ceea32b5
xgqfrms <xgqfrms@outlook.com>
2020-08-04 16:07:00 +0800

    import module all in one

--- Python-Data/demo.py
+++ Python-Data/demo.py
@@ -1,17 +1,38 @@
 # coding: utf8
 
-import camelcase
-# import CamelcasedModule as camelcased_module
-# import CamelCase as camelcase
+# import Module
+# 导入整个模块
+# import camelcase
 
-# ModuleNotFoundError: No module named 'Camelcase'
+# 模块名.函数名
+# c = camelcase.CamelCase()
+# txt = "hello world"
+# print(c.hump(txt))
 
-c = camelcase.CamelCase()
-# AttributeError: module 'camelcase' has no attribute 'CamelCase'
+# from Module import Function
+# 导入部分模块
+from camelcase import CamelCase
 
+函数名
+c = CamelCase()
 txt = "hello world"
-
 print(c.hump(txt))
-#This method capitalizes the first letter of each word.
 
+
+# import Module as Alias_Module
+# 导入整个模块, 并且使用 Alias 模块别名
+# import camelcase as cc
+
+# c = cc.CamelCase()
+# txt = "hello world"
+# print(c.hump(txt))
+
+
+"""bug
+
+  # ModuleNotFoundError: No module named 'Camelcase'
+  # AttributeError: module 'camelcase' has no attribute 'CamelCase'
+
+  # This method capitalizes the first letter of each word.
+"""
 
--- Python-Data/py3_dir.py
+++ Python-Data/py3_dir.py
@@ -27,3 +27,13 @@ __copyright__ = """
    *
   */
 """
+
+import platform
+
+dir = dir(platform)
+# List all the defined names belonging to the `platform` module:
+
+
+print(dir)
+
+# ['DEV_NULL', '_UNIXCONFDIR', '_WIN32_CLIENT_RELEASES', '_WIN32_SERVER_RELEASES', '__builtins__', '__cached__', '__copyright__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', '__version__', '_comparable_version', '_component_re', '_default_architecture', '_dist_try_harder', '_follow_symlinks', '_ironpython26_sys_version_parser', '_ironpython_sys_version_parser', '_java_getprop', '_libc_search', '_linux_distribution', '_lsb_release_version', '_mac_ver_xml', '_node', '_norm_version', '_parse_release_file', '_platform', '_platform_cache', '_pypy_sys_version_parser', '_release_filename', '_release_version', '_supported_dists', '_sys_version', '_sys_version_cache', '_sys_version_parser', '_syscmd_file', '_syscmd_uname', '_syscmd_ver', '_uname_cache', '_ver_output', '_ver_stages', 'architecture', 'collections', 'dist', 'java_ver', 'libc_ver', 'linux_distribution', 'mac_ver', 'machine', 'node', 'os', 'platform', 'popen', 'processor', 'python_branch', 'python_build', 'python_compiler', 'python_implementation', 'python_revision', 'python_version', 'python_version_tuple', 're', 'release', 'subprocess', 'sys', 'system', 'system_alias', 'uname', 'uname_result', 'version', 'warnings', 'win32_ver']
