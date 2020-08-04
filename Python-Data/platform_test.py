afbd71f965e2897a00128e0c55cedeca068f460b
xgqfrms <xgqfrms@outlook.com>
2020-08-04 10:48:58 +0800

    filname conflict bug

---
+++ Python-Data/platform_test.py
@@ -0,0 +1,11 @@
+
+import platform
+
+# x = platform.system()
+# print(x)
+# AttributeError: module 'platform' has no attribute 'system'
+
+print(platform)
+# <module 'platform' from '/Users/xgqfrms-mbp/Documents/GitHub/Python/Python-Data/platform.py'>
+
+# <module 'platform' from '/Applications/Xcode.app/Contents/Developer/Library/Frameworks/Python3.framework/Versions/3.7/lib/python3.7/platform.py'>
