725db25a903faef21a8f4a0d1e27a83ece4b1b3b
xgqfrms <xgqfrms@outlook.com>
2020-08-04 10:59:23 +0800

    Built-in Modules OK
    https://www.w3schools.com/python/python_modules.asp

--- Python-Data/platform_test.py
+++ Python-Data/platform_test.py
@@ -5,7 +5,31 @@ import platform
 # print(x)
 # AttributeError: module 'platform' has no attribute 'system'
 
-print(platform)
+print('\nuname =\n', platform.uname())
+
+print('\nversion =\n', platform.version())
+
+print('\nsystem =\n', platform.system())
+
+"""
+
+uname =
+ uname_result(system='Darwin', node='xgqfrms-mbp.local', release='19.5.0', version='Darwin Kernel Version 19.5.0: Tue May 26 20:41:44 PDT 2020; root:xnu-6153.121.2~2/RELEASE_X86_64', machine='x86_64', processor='i386')
+
+version =
+ Darwin Kernel Version 19.5.0: Tue May 26 20:41:44 PDT 2020; root:xnu-6153.121.2~2/RELEASE_X86_64
+
+system =
+ Darwin
+
+"""
+
+"""
+# module name conflict bug
+
+# print(platform)
 # <module 'platform' from '/Users/xgqfrms-mbp/Documents/GitHub/Python/Python-Data/platform.py'>
 
 # <module 'platform' from '/Applications/Xcode.app/Contents/Developer/Library/Frameworks/Python3.framework/Versions/3.7/lib/python3.7/platform.py'>
+
+"""
---
+++ Python-Data/py3_module.py
@@ -0,0 +1,25 @@
+# coding: utf8
+
+__author__ = 'xgqfrms'
+
+__version__ = '1.0.1'
+
+"""
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-01-0
+ *
+ * @description
+ * @augments
+ * @example
+ * @link
+ *
+ */
+
+"""
+
+
