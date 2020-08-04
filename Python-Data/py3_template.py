58340bc39355c42b44e6812cf1da9c3e4ba5e746
xgqfrms <xgqfrms@outlook.com>
2020-08-04 11:11:47 +0800

    Python3 Template update

---
+++ Python-Data/py3_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# coding: utf8
+
+__author__ = 'xgqfrms'
+
+__editor__ = 'vscode'
+
+__version__ = '1.0.1'
+
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+
+  Permission to use, copy, modify, and distribute this software and its
+  documentation for any purpose and without fee or royalty is hereby granted,
+  provided that the above copyright notice appear in all copies and that
+  both that copyright notice and this permission notice appear in
+  supporting documentation or portions thereof, including modifications,
+  that you make.
+
+  WEBGEEKER.XYZ SOFTWARE XRAY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+  THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+  FITNESS, IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL,
+  INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+  FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+  NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+  WITH THE USE OR PERFORMANCE OF THIS SOFTWARE !
+"""
+
+"""
+  /**
+  *
+  * @author xgqfrms
+  * @license MIT
+  * @copyright xgqfrms
+  * @created 2020-01-0
+  *
+  * @description
+  * @augments
+  * @example
+  * @link
+  *
+  */
+"""
+
+
