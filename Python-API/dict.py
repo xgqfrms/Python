55a18630ee53077a54c1c867f5cbb9694433536e
xgqfrms <xgqfrms@outlook.com>
2021-01-10 00:01:56 +0800

    dict ✅ 多行错误

--- Python-API/dict.py
+++ Python-API/dict.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
+# ✅ 一行 OK
 # dict = {'Name': 'xgqfrms', 'Age': 18, 'Class': 'First'}
-dict = {'Name': 'xgqfrms', 'Age': 18, 'Class': 'First'}
+
+# ✅ 多行错误
+dict = {
+  'Name': 'xgqfrms',
+  'Age': 18,
+  'Class': 'First',
+  'Country': 'China 🇨🇳',
+}
 
 # 更新 Age
 dict['Age'] = 23
 
 # 添加信息
-dict['School'] = "菜鸟教程"
-
+dict['School'] = "Python 教程"
 
 print ("dict['Age']: ", dict['Age'])
 print ("dict['School']: ", dict['School'])
+print ("dict['Country']: ", dict['Country'])
--- Python-API/get.py
+++ Python-API/get.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import json
+# ModuleNotFoundError: No module named 'url', 模块导入错误 ❌
 from url.parse import parse_qs
 from wsgiref.simple_server import make_server
 
@@ -29,5 +30,6 @@ if __name__ == "__main__":
   # 自定义开启的端口
   port = 5088
   httpd = make_server("0.0.0.0", port, application)
-  print("serving http on port {0}...").format(str(port))
+  print("serving http on port {0}...".format(str(port)))
+  # print("serving http on port {0}...").format(str(port))
   httpd.serve_forever()
