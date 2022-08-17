e7f3cf49a0c7402d69e75d5d3bf9e4ea5a4fa55b
xgqfrms <xgqfrms@outlook.com>
2022-08-18 01:58:19 +0800

    Create if-not.py

---
+++ python3.x/if-not.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# coding=utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+
+
+print(not 1 == 1)
+
+print(not 1 > 7)
+
+
+if not True:
+   print("1")
+elif not (1 + 1 == 3):
+   print("2")
+else:
+   print("3")
+
