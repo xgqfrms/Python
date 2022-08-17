9b2cf6f8fa84b0a3a4074053ec4d90ae99275417
xgqfrms <xgqfrms@outlook.com>
2022-08-17 18:14:29 +0800

    Create vars.py

---
+++ python3.x/vars.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#!/usr/bin/python3
+
+# coding: utf-8
+# coding: utf8
+# coding=utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+"""
+  /**
+   *
+   * @author xgqfrms
+   * @license MIT
+   * @copyright xgqfrms
+   * @created 2020-08-0
+   *
+   * @description
+   * @augments
+   * @example
+   * @link
+   *
+  */
+"""
+
+
+
+# variable & case sensitive language
+
+usename = 'xgqfrms'
+
+UserName = 'xgqfrms'
+
+# Python 字符串插值
+name = "xgqfrms"
+age = "23"
+
+print(name + ' is ' + age + ' years old')
+
+# https://www.runoob.com/python3/python3-string.html
+
+
+
+
