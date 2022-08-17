3a87570028f36523decdf2a02f0b9c588978de7d
xgqfrms <xgqfrms@outlook.com>
2022-08-18 00:28:17 +0800

    Create if-or.py

---
+++ python3.x/if-or.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# coding=utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+
+
+purity = float(input())
+#your code goes here
+
+if(purity >= 91.7 or purity >= 99.9) :
+    print("Accepted")
+# 22K gold has 91.7% or more gold in it, while 24K corresponds to 99.9% and above purity.
+
+
+# https://www.sololearn.com/learning/1157
+
+
+"""
+
+行与缩进
+python最具特色的就是使用缩进来表示代码块，不需要使用大括号 {} 。
+
+缩进的空格数是可变的，但是同一个代码块的语句必须包含相同的缩进空格数。实例如下
+
+if True:
+    print ("True")
+else:
+    print ("False")
+
+https://www.runoob.com/python3/python3-basic-syntax.html
+
+https://www.runoob.com/python3/python3-conditional-statements.html
+
+"""
+
+
+"""
+
+Python逻辑运算符
+Python语言支持逻辑运算符，以下假设变量 a 为 10, b为 20:
+
+运算符	逻辑表达式	描述	实例
+and	x and y	布尔"与" - 如果 x 为 False，x and y 返回 x 的值，否则返回 y 的计算值。	(a and b) 返回 20。
+or	x or y	布尔"或" - 如果 x 是 True，它返回 x 的值，否则它返回 y 的计算值。	(a or b) 返回 10。
+not	not x	布尔"非" - 如果 x 为 True，返回 False 。如果 x 为 False，它返回 True。	not(a and b) 返回 False
+
+
+https://www.runoob.com/python3/python3-basic-operators.html#ysf4
+
+
+
+
+
+"""
