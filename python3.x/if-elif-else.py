951a31a9a7592b1da49a4539f60e2c4b69caf61c
xgqfrms <xgqfrms@outlook.com>
2022-08-18 00:44:39 +0800

    Create if-elif-else.py

---
+++ python3.x/if-elif-else.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# coding=utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+
+score = 70
+
+if score < 60:
+   print("C")
+elif score < 85:
+   print("B")
+else:
+   print("A")
