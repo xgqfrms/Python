ee9c0c37ff1ae8aac7f64e618a7ff7f6ae88cf53
xgqfrms <xgqfrms@outlook.com>
2022-08-17 23:17:04 +0800

    Create input.py

---
+++ python3.x/input.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# coding: utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+"""
+  /**
+   *
+   * @author xgqfrms
+   * @license MIT
+   * @copyright xgqfrms
+   * @created 2022-08-17
+   *
+   * @description
+   * @augments
+   * @example
+   * @link https://www.sololearn.com/learning/1157
+   *
+  */
+"""
+
+
+str = input()
+print(f"*** {str} ***")
+
