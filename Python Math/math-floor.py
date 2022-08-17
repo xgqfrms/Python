a9ca9f0e693c1b9b6db44cfb06cc2e37426652d6
xgqfrms <xgqfrms@outlook.com>
2022-08-17 16:12:52 +0800

    Create math-floor.py

---
+++ Python Math/math-floor.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+#your code goes here
+# / 整除， 整数转成浮点数
+print(888 / 60)
+# 14.8
+
+# // 向下取整除
+print(888 // 60)
+# 14
+
+print(888 % 60)
+# 48
+
+
+'''
+多行注释（单引号）
+
+https://www.sololearn.com/learning/1157
+
+
+https://www.runoob.com/python/python-operators.html#ysf1
+
+运算符	描述	实例
++	加 - 两个对象相加	a + b 输出结果 30
+-	减 - 得到负数或是一个数减去另一个数	a - b 输出结果 -10
+*	乘 - 两个数相乘或是返回一个被重复若干次的字符串	a * b 输出结果 200
+/	除 - x除以y	b / a 输出结果 2
+%	取模 - 返回除法的余数	b % a 输出结果 0
+**	幂 - 返回x的y次幂	a**b 为10的20次方， 输出结果 100000000000000000000
+//	取整除 - 返回商的整数部分（向下取整）	
+>>> 9//2
+4
+>>> -9//2
+-5
+
+
+
+https://www.runoob.com/python3/python3-number.html
+
+
+
+NumPy 数学函数
+NumPy 包含大量的各种数学运算的函数，包括三角函数，算术运算的函数，复数处理函数等。
+
+https://www.runoob.com/numpy/numpy-mathematical-functions.html
+
+```
+
+“”“
+多行注释（双引号）
+
+https://www.runoob.com/python3/python3-comment.html
+
+“”“
+
+# 单行注释
