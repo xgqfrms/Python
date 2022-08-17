c9fef09a3555c972515354181c6e438b6bd85be4
xgqfrms <xgqfrms@outlook.com>
2022-08-17 17:13:10 +0800

    Create author.py

---
+++ python3.x/author.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+__author__ = 'xgqfrms'
+
+# coding: utf-8
+# coding=utf-8
+
+# 不需要分号
+# print('Python 3 🐍🐍🐍');
+print('Python 3 🐍🐍🐍')
+
+print("Python 3 🐍")
+
+# 单行注释
+
+"""
+
+多行注释（双引号）
+
+"""
+
+'''
+
+多行注释（单引号）
+
+'''
