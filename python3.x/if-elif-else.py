6ff8290f5cf41a1415fb85ee576d1829790c2ced
xgqfrms <xgqfrms@outlook.com>
2022-08-18 00:59:48 +0800

    24K

--- python3.x/if-elif-else.py
+++ python3.x/if-elif-else.py
@@ -18,3 +18,44 @@ elif score < 85:
    print("B")
 else:
    print("A")
+
+
+
+
+purity = float(input())
+#your code goes here
+
+
+if(purity >= 99.9):
+    print("24K")
+elif(purity >= 91.7 and purity < 99.9):
+    print("22K")
+elif(purity >= 83.3 and purity < 91.7):
+    print("20K")
+elif(purity >= 75.0 and purity < 83.3):
+    print("18K")
+
+
+'''
+if(purity >= 99.9):
+    print("24K")
+elif(purity >= 91.7 and purity < 99.9):
+    print("24K")
+elif(purity >= 83.3 and purity < 91.7):
+    print("20K")
+elif(purity >= 75 and purity < 83.3):
+    print("18K")
+
+
+'''
+
+
+"""
+
+
+24K – 99.9%
+22K – 91.7%
+20K – 83.3%
+18K – 75.0%
+
+"""
