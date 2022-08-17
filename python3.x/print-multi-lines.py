cadf991f4f6e3ec404f36435b4f83293f6116169
xgqfrms <xgqfrms@outlook.com>
2022-08-17 16:31:11 +0800

    Create print-multi-lines.py

---
+++ python3.x/print-multi-lines.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+
+print("""
+this
+is a
+multiline
+text
+""") 
+
+"""
+
+多行注释（双引号）
+
+js console.log(`
+this
+is a
+multiline
+text
+`);
+
+"""
+
+'''
+
+
+多行注释（单引号）
+
+https://www.runoob.com/python3/python3-comment.html
+
+
+js console.log(`
+this
+is a
+multiline
+text
+`);
+
+'''
