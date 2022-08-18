1fd3d0dd305ca26df2d5588530c25a05f0744a6a
xgqfrms <xgqfrms@outlook.com>
2022-08-18 17:39:44 +0800

    Create list-range.py

---
+++ python3.x/list-range.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+"""
+  /**
+   *
+   * @author xgqfrms
+   * @license MIT
+   * @copyright xgqfrms
+   * @created 2022-08-17
+   *
+   * @description
+   * @augments
+   * @example
+   * @link
+   *
+  */
+"""
+
+
+
+n = int(input())
+#your code goes here
+
+restrooms = list(range(5, n, 5))
+
+# print(restrooms)
+for restroom in restrooms:
+  print(restroom)
+  
+"""
+
+
+n = int(input())
+#your code goes here
+
+restrooms = list(range(0, n, 5))
+
+# print(restrooms)
+for restroom in restrooms:
+  if(restroom >= 5):
+    print(restroom)
+  
+
+
+"""
+  
