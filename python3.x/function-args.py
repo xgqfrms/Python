1b7093c199f85a125ba1edf42f978520aacbd647
xgqfrms <xgqfrms@outlook.com>
2022-08-19 08:45:15 +0800

    Create function-args.py

---
+++ python3.x/function-args.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+"""
+  /**
+   *
+   * @author xgqfrms
+   * @license MIT
+   * @copyright xgqfrms
+   * @created 2022-08-18
+   *
+   * @description
+   * @augments
+   * @example
+   * @link
+   *
+  */
+"""
+
+
+def test(a, b, c):
+  print(a, b, c)
+  # print(args)
+  # print(arguments)
+  
+# NameError: name 'args' is not defined
+# NameError: name 'arguments' is not defined
+
+test(1,2,3)
+# 1 2 3
+
+# test(1,2,3,4)
+# TypeError: test() takes 3 positional arguments but 4 were given
+
+# https://www.runoob.com/python/python-functions.html
