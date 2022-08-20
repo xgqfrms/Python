afcb2a594082704f10267be96cdc8c86a662b524
xgqfrms <xgqfrms@outlook.com>
2022-08-21 00:47:17 +0800

    Update func-string.py

--- python3.x/func-string.py
+++ python3.x/func-string.py
@@ -14,3 +14,21 @@ text = input()
 letter = input()
 
 print(letter_count(text, letter))
+
+
+
+
+def search(word, text):
+  if(word.find(text) > -1):
+    return "Word found"
+  else:
+    return "Word not found"
+
+text = input()
+word = input()
+print(search(text, word))
+
+
+"""
+The search() function should return "Word found" if the word is present in the text, or "Word not found", if it’s not.
+"""
