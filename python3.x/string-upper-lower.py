08f31be68bbc91061124a7a3fdc9c2a110b4dd7a
xgqfrms <xgqfrms@outlook.com>
2022-08-21 00:40:47 +0800

    Create string-upper-lower.py

---
+++ python3.x/string-upper-lower.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+#your code goes here
+
+message = input()
+
+# uppercase => lowercase
+print(message.lower())
+
+
+# https://www.runoob.com/try/runcode.php?filename=helloworld_cn&type=python
+
+
+
+uppercase = "uppercase".upper()
+print(uppercase)
+
+print("\n")
+# uppercase => lowercase
+
+# lowercase = "lowercase".upper()
+lowercase = "LOWERCASE".lower()
+print(lowercase)
+
+
+"""
+
+UPPERCASE
+
+
+lowercase
+
+
+"""
+
