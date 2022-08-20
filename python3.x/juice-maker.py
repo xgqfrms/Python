322b0242baeb34b9b751f7624b28952ba4288c55
xgqfrms <xgqfrms@outlook.com>
2022-08-21 02:04:48 +0800

    Create juice-maker.py

---
+++ python3.x/juice-maker.py
@@ -0,0 +1,58 @@
+
+# https://www.sololearn.com/learning/eom-project/1073/357
+
+
+# class Juice:
+#     def __init__(self, name, capacity):
+#         self.name = name
+#         self.capacity = capacity
+
+#     def __str__(self):
+#         return (self.name + ' ('+str(self.capacity)+'L)')
+#     # 改写 add
+#     def _add__(self, other):
+#         names = Juice(self.name) + '&' + Juice(other.name)
+#         capacity = ' (' + str(Juice(self.capacity + Juice(other.capacity)) + 'L)'
+#         return Juice(names + capacity)
+#         # return f"{self.name}&{other.name}({self.capacity + other.capacity}L)"
+
+class Juice:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
+    def __str__(self):
+        return (self.name + ' ('+str(self.capacity)+'L)')
+    # 改写 add
+    def _add__(self, other):
+        return (Juice(self.name + '&' + other.name + ' (' + str(self.capacity + other.capacity) + 'L)'))
+        # return f"{self.name}&{other.name}({self.capacity + other.capacity}L)"
+
+a = Juice('Orange', 1.5)
+b = Juice('Apple', 2.0)
+
+# print(a)
+# print(a.name)
+
+def add(a, b):
+    return f"{a.name}&{b.name}({a.capacity + b.capacity}L)"
+
+# result = add(a, b)
+result = a + b
+print(result)
+
+
+
+
+"""
+python cutsom __add__
+
+
+https://www.codingem.com/python-__add__-method/
+
+
+https://stackoverflow.com/questions/46885618/using-add-operator-with-multiple-arguments-in-python
+
+
+
+"""
