b301ee2b7e20715261a6aee1a7636f38ffc6c6e7
xgqfrms <xgqfrms@outlook.com>
2022-08-28 23:03:30 +0800

    Create py3.py

---
+++ python3.x/py3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# coding=utf-8
+
+__author__ = 'xgqfrms'
+__editor__ = 'vscode'
+__version__ = '1.0.1'
+__copyright__ = """
+  Copyright (c) 2012-2050, xgqfrms; mailto:xgqfrms@xgqfrms.xyz
+"""
+
+"""
+  /**
+   * 
+   * @author xgqfrms
+   * @license MIT
+   * @copyright xgqfrms
+   * @created 2020-08-0
+   * 
+   * @description 
+   * @augments 
+   * @example 
+   * @link 
+   * 
+  */
+"""
+
+print('py3 code template 🐍')
